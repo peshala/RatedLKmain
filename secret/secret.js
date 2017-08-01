90558a74dc1df1767d517a4e917dde0ec7011e58
Peshala <peshaladayanatha@gmail.com>
2017-08-01 20:35:32 +0530

    Add starting files to manage the sign in and singup

---
+++ secret/secret.js
@@ -0,0 +1,9 @@
+module.exports = {
+    facebook:{
+        clientID:'720490264803956',
+        clientSecret:'74a2d281776667bef03a1a8d026f58f7',
+        profileFeilds:['email','displayName'],
+        callbackURL:'http://localhost:3000/auth/facebook/callback',
+        passReqToCallback:true
+    }
+}
